483d7895260406265e4d9e050cda606cc3f0295d
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-05-05 01:08:28 -0300

    Update examples.clj

--- examples.clj
+++ examples.clj
@@ -1,1 +1,9 @@
 (pr "Hola mundo")
+
+;; return the third angle of a triangle
+(defn tercer-angulo [x y] (- 180 (+ x y)))
+(tercer-angulo 100 20)
+
+;; return a day in seconds
+(defn segundos [d h m s] (+ (* (* (* d 24) 60) 60) (* (* h 60) 60) (* m 60) s))
+(segundos 1 1 1 1)

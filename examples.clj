8c8953dec2b913227499a182b5948f3bc42ad671
Joana Piovaroli <joanapiovaroli@gmail.com>
2025-05-05 12:16:50 -0300

    Update examples.clj

--- examples.clj
+++ examples.clj
@@ -7,3 +7,11 @@
 ;; return a day in seconds
 (defn segundos [d h m s] (+ (* d 24 60 60) (* h 60 60) (* m 60) s))
 (segundos 1 1 1 1)
+
+;; get the first multiple of 10
+(defn sig-mul-10 [i]
+  (if (= (mod i 10) 0)
+  i
+  (recur (inc i))
+  ))
+  
